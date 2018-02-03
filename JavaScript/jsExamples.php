7e3f2d66973693007ce09282f3099b247ef15c62
David-Christopher <dav08049@byui.edu>
2018-02-03 14:54:47 -0700

    Add files via upload

---
+++ JavaScript/jsExamples.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta charset="utf-8"/>
+  <title>JavaScript Examples</title>
+  <link rel="stylesheet" type="text/css" href="../css/examplesStyle.css">
+</head>
+<body>
+
+  <nav id="main-navigation">
+  </nav>
+
+  <header>
+    <h1>JavaScript Examples</h1>
+  </header>
+
+  <table>
+    <tr><td><strong>JavaScript Pages</strong></td><td><strong>Description</strong></td></tr>
+  
+    <tr><td><a href="acreconverter.html">Acre Converter</a></td><td>This code includes functions and variables to recieve user input and convert the amount into acres.</td></tr>
+  
+    <tr><td><a href="addingintegers.html">Add Up Integers</a></td><td>This code includes functions, variables, conditional statements, and parameters to receive a number from the user and add up all integers between 1 and the user's number.</td></tr>
+  
+    <tr><td><a href="compoundinterest.html"> Calculate Compound Interest</a></td><td>This code includes functions, variables, and for loop to calculate compound interest from user's input.</td></tr>
+  
+    <tr><td><a href="testyourmath.html">Test Your Math Skills</a></td><td>This code includes functions, variables, and switch statement to calculate user's input and varify if the user's answer is correct.</td></tr>
+
+  </table>
+    <br><br>
+
+  <p>Click on a link above to further inspect my JavaScript examples!</p>
+
+</body>
+</html>
